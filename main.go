6d84f3b51c7c257386b62f12dabd6512bda806dc
RulerOfCakes <fragmentbombs@gmail.com>
2023-07-06 01:17:26 +0900

    feat: initialize project

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var (
+	Token string
+)
+
+func init() {
+	flag.StringVar(&Token, "t", "", "Bot Token")
+	flag.Parse()
+}
+
+func main() {
+	discord, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		fmt.Errorf("Error creating Discord session: %v", err)
+		return
+	}
+}
